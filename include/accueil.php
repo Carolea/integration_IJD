911025126b2baaf49c8f120dafcf3a818d957075
Carole <carole.ambert@lepoles.com>
2017-11-09 15:19:57 +0100

    Add files via upload

---
+++ include/accueil.php
@@ -0,0 +1,209 @@
+<!--Slider top-->
+<div class="nav_background">
+    <img src="img/bandeau-bonplan.jpg" alt="">
+</div>
+<!---->
+
+
+<!--Barre de recherche-->
+
+<div id='search-box'>
+
+    <form action='/search' id='search-form' method='get' target='_top'>
+
+        <input id="search-text" name="q" placeholder="A la recherche d'un bon plan?" size="50" type="text" />
+
+        <button id='search-button' type='submit'><span>C'est parti</span></button>
+
+    </form>
+</div>
+
+<!--/-->
+<div class="conteneur">
+
+
+    <!--Slider bons plans-->
+    <h2>Bons plans du moment</h2>
+
+    <div class="bon-plan">
+        <figure>
+            <a href="http://www.cartejeunes.fr/bon-plan/jeux-concours/">   
+        <img src="img/accueil/JEU.jpg" alt="Visuel Jeux Concours"></a>
+
+            <figcaption class="right">
+                <!--logo-->
+                <img src="img/accueil/LOGOJCnoir.png" alt="">
+                <!--offre-->
+                <p>Accédez aux Jeux-concours du moment !</p>
+                <span class="sub-picto"> en savoir +</span>
+            </figcaption>
+
+            <!--Décompte avant fin de l'offre-->
+
+            <figcaption class="under"></figcaption>
+        </figure>
+        <figure>
+            <a href="http://www.cartejeunes.fr/bon-plan/air-europa/">         
+        <img src="img/accueil/vignette-10.png" alt="Visuel bon plan"></a>
+            <div class="countdown">4 semaines 1 jour 00:55:34</div>
+
+            <figcaption class="right">
+                <img src="img/accueil/logo-noir-3.png" alt="logo">
+                <p>10% de réduction sur votre vol avec Air Europa</p>
+                <span class="sub-picto"> en savoir +</span>
+                <p>"Jusqu'au 08/12/2017 12:00 "</p>
+
+                <!--Décompte avant fin de l'offre-->
+            </figcaption>
+        </figure>
+    </div>
+    <!---->
+    <div class="cartepicto">
+        <a class="button" title="Voir tous les bons plans " href="http://www.cartejeunes.fr/nos-bons-plans ">Voir tous les bons plans</a>
+    </div>
+
+    <!--Slider top avantages-->
+
+    <h2>Top Avantages</h2>
+
+    <div class="thumbnail">
+        <a href="">
+						<img src="img/accueil/vedettedeparis.png" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+        <div class="etiquette">
+            -50% Frère
+        </div>
+        <div class="etiquette_2">
+            -30% Frère
+        </div>
+        <div class="etiquette_3">
+            -30% Frère
+        </div>
+    </div>
+
+    <div class="thumbnail">
+        <a href="">
+						<img src="img/accueil/lost.png" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+        <div class="etiquette">
+            -50% Frère
+        </div>
+        <div class="etiquette_2">
+            -30% Frère
+        </div>
+        <div class="etiquette_3">
+            -30% Frère
+        </div>
+    </div>
+
+    <div class="thumbnail">
+        <a href="">
+						<img src="img/accueil/kanoak.png" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+        <div class="etiquette">
+            -50% Frère
+        </div>
+        <div class="etiquette_2">
+            -30% Frère
+        </div>
+        <div class="etiquette_3">
+            -30% Frère
+        </div>
+    </div>
+
+    <div class="thumbnail">
+        <a href="">
+						<img src="img/accueil/zaragoza.png" alt="" class="thumbnail">
+
+						<div class="surcouche">
+							<div>
+								Une place de tennis offerte pour l'achat d'une place de concert
+							</div>
+						</div>
+					</a>
+        <div class="etiquette">
+            -50% Frère
+        </div>
+        <div class="etiquette_2">
+            -30% Frère
+        </div>
+        <div class="etiquette_3">
+            -30% Frère
+        </div>
+    </div>
+
+    <div class="cartepicto">
+
+        <a class="button" title="Voir tous les top avantages " href="http://www.cartejeunes.fr/nos-bons-plans/nos-avantages/ ">Voir tous les top avantages</a>
+    </div>
+</div>
+<div>
+
+    <!---->
+
+    <!--API Twitter-->
+    <div class="api-twitter">
+
+        <div class="icon-twitter">
+            <a href="https://twitter.com/CarteJeunesFr" target="_blank"><i class="fa fa-twitter"></i></a>
+        </div>
+
+    </div>
+
+    <!---->
+
+
+    <!--Picto-->
+    <h2>LA CARTE JEUNES EUROPÉENNE</h2>
+
+    <div class="cartepicto ">
+        <div class="picto ">
+            <img src="img/obtenir-la-carte/picto-gift.png " class="picto " alt="picto-cadeaux " /><br>
+            <span class="bold sub-picto ">60 000+</span>
+            <br />
+            <span class="bold ">Bons plans pour les jeunes entre 12 et 30 ans</span>
+        </div>
+
+        <div class="picto ">
+            <img src="img/obtenir-la-carte/picto-millions.png " class="picto " alt="picto-membres " /><br>
+            <span class="bold sub-picto ">6 MILLIONS</span>
+            <br />
+            <span class="bold ">De jeunes déjà possesseurs de la carte en Europe</span>
+        </div>
+
+        <div class="picto ">
+            <img src="img/obtenir-la-carte/picto-pays.png " class="picto " alt="picto-pays " /><br>
+            <span class="bold sub-picto ">37 PAYS</span>
+            <br />
+            <span class="bold ">Où la carte est utilisable et ce, quel que soit le pays d’obtention</span>
+        </div>
+
+        <div class="picto ">
+            <img src="img/obtenir-la-carte/picto-annees.png " class="picto " alt="picto-anniversaire " /><br>
+            <span class="bold sub-picto ">29 ANS</span>
+            <br />
+            <span class="bold ">Depuis la création de la EYCA (European Youth Card Association)</span>
+        </div><br>
+
+        <a class="button" title="Découvrir la carte " href="http://www.cartejeunes.fr/la-carte ">Découvrir la carte</a>
+    </div>
+
+</div>
